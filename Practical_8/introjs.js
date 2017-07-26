06ca7d7851931cad6f304db5ba463d35b400e9a6
Kamikid <kidfu@live.com>
2017-07-26 18:08:49 +1200

    Add practical 8

---
+++ Practical_8/introjs.js
@@ -0,0 +1,6 @@
+
+    alert(window.navigator.userAgent);
+	var e = window.navigator.userAgent;
+	if(e.includes(".NET")){ alert("Not Support");}
+	console.log("Screen width: " + window.screen.width);
+	
